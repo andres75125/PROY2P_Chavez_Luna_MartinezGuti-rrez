fc8b16a21ad0f4da7875d4fb41395473420d89e3
Andrés Martínez Gutiérrez <starlord52mcu@gmail.com>
2026-08-16 00:35:31 -0500

    Update Usuario.java

--- app/src/main/java/com/example/proy2p_chavez_luna_martnezgutierrez/Usuario.java
+++ app/src/main/java/com/example/proy2p_chavez_luna_martnezgutierrez/Usuario.java
@@ -1,1 +1,23 @@
+package com.espol.pronosticosmundial;
 
+public abstract class Usuario {
+    protected String idUsuario;
+    protected String nombreUsuario;
+    protected String contrasena;
+    protected String nombreCompleto;
+    protected String tipoUsuario;
+
+    public Usuario(String idUsuario, String nombreUsuario, String contrasena, String nombreCompleto, String tipoUsuario) {
+        this.idUsuario = idUsuario;
+        this.nombreUsuario = nombreUsuario;
+        this.contrasena = contrasena;
+        this.nombreCompleto = nombreCompleto;
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    public String getIdUsuario() { return idUsuario; }
+    public String getNombreUsuario() { return nombreUsuario; }
+    public String getContrasena() { return contrasena; }
+    public String getNombreCompleto() { return nombreCompleto; }
+    public String getTipoUsuario() { return tipoUsuario; }
+}
