44007e3a390da356c7dc4f2031e40cd08268f1c1
Andrés Martínez Gutiérrez <starlord52mcu@gmail.com>
2026-08-16 00:54:07 -0500

    Update Administrador.java

--- app/src/main/java/com/example/proy2p_chavez_luna_martnezgutierrez/Administrador.java
+++ app/src/main/java/com/example/proy2p_chavez_luna_martnezgutierrez/Administrador.java
@@ -1,1 +1,11 @@
+package com.example.proy2p_chavez_luna_martnezgutirrez;
+public class Administrador extends Usuario {
+    private String cargo;
 
+    public Administrador(String idUsuario, String nombreUsuario, String contrasena, String nombreCompleto, String tipoUsuario, String cargo) {
+        super(idUsuario, nombreUsuario, contrasena, nombreCompleto, tipoUsuario);
+        this.cargo = cargo;
+    }
+
+    public String getCargo() { return cargo; }
+}
